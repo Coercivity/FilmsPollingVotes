c171bb75ed0c3058a2f658183a2292dc8e8c7cb7
Coercivity <digitalpirouette@gmail.com>
2022-07-01 12:17:30 +0300

    Implement authorization controller with view

---
+++ src/MVC/LobbyMVC/Controllers/AuthorizationController.cs
@@ -0,0 +1,30 @@
+﻿using LobbyMVC.Dtos;
+using LobbyMVC.Services.IdentityService;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace LobbyMVC.Controllers
+{
+    public class AuthorizationController : Controller
+    {
+        private readonly IIdentityService _identityService;
+
+        public AuthorizationController(IIdentityService identityService)
+        {
+            _identityService = identityService;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        public async Task<IActionResult> AuthroizeAsync(AuthentificateUserDto authentificateUserDto)
+        {
+            var claims = await _identityService.AuthorizeUserAsync(authentificateUserDto);
+            return Redirect("/");
+        }
+
+    }
+}
